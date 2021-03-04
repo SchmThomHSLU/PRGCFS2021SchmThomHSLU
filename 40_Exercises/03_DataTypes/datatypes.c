88e4b5bbb2a5f2aceb58f436f00b013c7901e58e
SchmThomHSLU <thomas.schmid@stud.hslu.ch>
2021-03-04 20:23:48 +0100

    afdfas

---
+++ 40_Exercises/03_DataTypes/datatypes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("size of char: %ld byte(s).\n",sizeof(char) );
+	printf("size of uchar: %ld byte(s).\n",sizeof(unsigned char) );
+	printf("size of short: %ld byte(s).\n",sizeof(short) );
+	printf("size of int: %ld byte(s).\n",sizeof(int) );
+	printf("size of long: %ld byte(s).\n",sizeof(long) );		
+	printf("size of long long: %ld byte(s).\n",sizeof(long long) );	
+	printf("size of double: %ld byte(s).\n",sizeof(double) );	
+	printf("size of float: %ld byte(s).\n",sizeof(float) );
+	printf("size of long double: %ld byte(s).\n",sizeof(long double) );
+
+	
+	return 0;
+}
