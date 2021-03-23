37c24f4a3e00a3c661f9d632780776b784a6fb7e
SchmThomHSLU <thomas.schmid@stud.hslu.ch>
2021-03-23 19:52:18 +0100

    Update 23.03.21

--- 40_Exercises/04_CLA_Operators/logicOperators.c
+++ 40_Exercises/04_CLA_Operators/logicOperators.c
@@ -1,18 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[] )
 {
 
-	//falls nicht 3 Argumente  --> hier hinein  Zeile 7-10
-	if( argc != 3 ){
+	//falls nicht 4 Argumente  --> hier hinein  Zeile 7-10
+	if( argc != 4 )
+	{
 		//input is not valid
 		printf("Usage:\n");
-		printf("logicOperators Input1 Input2\n");
+		printf("logicOperators Input1 Input2 Input3\n");
 	}
 	else
 	
 	{	
-		// Input is valid  --> genau 3 Argumente erhalten
+		// Input is valid  --> genau 1+3 Argumente erhalten
+		int val1 = atoi(argv[1]); 
+		int val2 = atoi(argv[2]); 
+		int val3 = atoi(argv[3]); 
+		int sum = 0;
+		float avg = 0.0f;
+		
+		printf("%d, %d, %d\n", val1, val2, val3);
+		
+		
+		// find the greatest
+		if( !(val1 > 0) || !(val2 > 0) || !(val3 > 0))
+		{
+			// Fehlerfall
+			printf("All inputs must be strictly greater than zero.\n");
+		}
+		else
+		{
+			if(val1 >= val2 )
+			{
+				if( val1 >= val3 )
+				{
+					// val1 is the greatest
+					printf("%d is the greatest\n", val1);
+				}
+				else
+				{
+					// val3 is the gratest
+					printf("%d is the greatest\n", val3);
+				}
+			}
+			else
+			{
+				if( val2 >= val3 )
+				{
+					// val2 is the greatest
+					printf("%d is the greatest\n", val2);
+				}
+				else
+				{
+					// val3 is the gratest
+					printf("%d is the greatest\n", val3);
+				}
+			}
+		
+			// find the smalest
+			if(val1 <= val2 && val1 <= val3)
+			{
+			 printf("%d is the smallest\n", val1);
+			}
+			else if(val2 <= val1 && val2 <= val3)
+			{
+			 printf("%d is the smallest\n", val2);
+			}
+			else
+			{
+			 printf("%d is the smallest\n", val3);
+			}
+			
+			//summe of al values
+			sum = (val1 + val2 + val3);
+			printf("The sum is: %d\n", sum );
+			
+			//average of all values
+			avg = (float)sum/3;
+			printf("The average is: %.2f\n", avg );
+		}
+	}
+	
+	
+		
+#if 0		//auskommentieren mit Präprozessor		
+		
+		
 		printf("Number of input arguments: %d\n", argc );	//Anzahl args. printen
 	
 	//Die drei Elemente in der For-Loop Deklaration:
@@ -29,6 +104,8 @@ int main(int argc, char *argv[] )
 		// 		- 23
 		//		- 12cd ..ge
 	}
-	}
+}
+#endif
+	
 	return 0;
 }	
